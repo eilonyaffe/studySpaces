41d8ed9de3e2ae8f809b7a6caf689d8fd8d79965
eilon <eilonyaffe3@gmail.com>
2025-06-08 17:58:00 +0300

    github actions

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const SEMESTER:string = '2';  // this is for the server. the scraper uses shell input
-export const RETRY:boolean = true;  // controls if we retry scraping courses that weren't successfully scraped the first time TODO change later as environment var
+export const RETRY:boolean = false;  // controls if we retry scraping courses that weren't successfully scraped the first time TODO change later as environment var
 export const DISTANCE_FUNC:string = "euclidian"; // controls how we sort the results. either "euclidian" or "haversine"
